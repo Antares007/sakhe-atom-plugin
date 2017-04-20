de66dd95a05c690e32b84ca929714bef0d24d696
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-04-20 14:52:49 +0400

    ramda

--- t2.js
+++ t2.js
@@ -1,30 +1,47 @@
 const debug = require('debug')('main') // eslint-disable-line
+const R = require('ramda')
 const Main = require('./lib/main')
 const most = require('most')
-const h = require('snabbdom/h').default
+const {div, input, h1, b, button, span} = require('./lib/hyperscript-helpers')
 const isolate = require('./lib/isolate')
+// const Type = require('union-type')
 
-module.exports = function mainFunctionMonadLaws (sources) {
+module.exports = function ({DOM}) {
+  const name$ = DOM
+    .filter(R.whereEq({action: 'input'}))
+    .map(R.path(['event', 'target', 'value']))
+    .startWith('')
+  return {
+    DOM: name$.map(text =>
+      div([
+        input({props: {type: 'text'}, on: {input: 'input'}}),
+        h1('hello ' + text)
+      ])
+    )
+  }
+}
+
+function mainFunctionMonadLaws (sources) { // eslint-disable-line
   const M = (main) => Main.of(main)
   const bind = (M, f) => Main.chain(f, M)
   const unit = Main.of
 
   const f = (main) => unit((sources) => ({
     DOM: most.combine(
-      (...vdoms) => h('div', ['f(', ...vdoms, ')']),
+      (...vdoms) => div([span('f('), ...vdoms, span(')')]),
       main(sources).DOM,
       main(sources).DOM
     )
   }))
   const g = (main) => unit((sources) => ({
     DOM: most.combine(
-      (...vdoms) => h('div', ['g(', ...vdoms, ')']),
+      (...vdoms) => div([span('g('), ...vdoms, span(')')]),
       main(sources).DOM,
       main(sources).DOM,
       main(sources).DOM
     )
   }))
-  const _42 = (sources) => ({ DOM: most.of(h('b', '42')) })
+  const _42 = (sources) => ({ DOM: most.of(b('42')) })
   const laws = [
     [ bind(M(_42), f), f(_42) ],
     [ bind(M(_42), unit), M(_42) ],
@@ -35,12 +52,12 @@ module.exports = function mainFunctionMonadLaws (sources) {
   )(sources)
 }
 
-function b (sources) { // eslint-disable-line
+function b_ (sources) { // eslint-disable-line
   const sinks1 = isolate(c)(sources)
   const sinks2 = isolate(c)(sources)
   return {
     DOM: most.combine(function (vdom1, vdom2, v1, v2) {
-      return h('div', [vdom1, vdom2, h('p', v1 + v2)])
+      return div([vdom1, vdom2, b(v1 + v2)])
     }, sinks1.DOM, sinks2.DOM, sinks1.value$, sinks2.value$)
   }
 }
@@ -54,10 +71,10 @@ function c ({DOM}) {
   return {
     DOM: value$
       .map(i =>
-        h('div.c', [
-          h('button', {on: {click: '-1'}}, '-1'),
-          h('button', {on: {click: '1'}}, '+1'),
-          h('p', i)
+        div('.c', [
+          button({on: {click: '-1'}}, '-1'),
+          button({on: {click: '1'}}, '+1'),
+          b(i)
         ])
       ),
     value$
@@ -69,7 +86,7 @@ function vertical (progs) { // eslint-disable-line
     return function (sources) {
       return {
         DOM: most.combineArray(
-          (...vdoms) => h('div.vertical', vdoms.map((vdom) => h('div.item', [vdom]))),
+          (...vdoms) => div('.vertical', vdoms.map((vdom) => div('.item', [vdom]))),
           mains.map((m) => m(sources).DOM)
         )
       }
@@ -87,12 +104,8 @@ function horizontal (progs) { // eslint-disable-line
         justifyContent: 'space-around'
       }
       return {
-        DOM: most.combineArray((...vdoms) =>
-          h(
-            'div',
-            { style },
-            vdoms.map((vdom) => h('div', [vdom]))
-          ),
+        DOM: most.combineArray(
+          (...vdoms) => div({ style }, vdoms),
           mains.map((m) => m(sources).DOM)
         )
       }
@@ -105,7 +118,7 @@ function shadow (prog) { // eslint-disable-line
     return function (sources) {
       return {
         DOM: main(sources).DOM.map((vdom) =>
-          h('div.shadow', {
+          div('.shadow', {
             style: {
               margin: '4px',
               padding: '5px',

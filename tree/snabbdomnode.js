a180bb8faa7c3ce62be17867dc9e6e5de0c6a201
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-05-15 10:16:57 +0400

    snabbdomnode

--- tree/snabbdomnode.js
+++ tree/snabbdomnode.js
@@ -1,16 +1,27 @@
+const snabbdom = require('snabbdom')
+const {curry2} = require('@most/prelude')
+const h = snabbdom.h
+
 const m = require('most')
 const rootNode = require('./rootnode')
-const identity = as$ => m.combineArray((...as) => as, as$)
+const setKetByIndex = (vnode, i) => Object.assign({}, vnode, {key: i})
+const identity = curry2(m.combineArray)(
+  (...as) => h('div', as.map(setKetByIndex))
+)
 const tree$ = rootNode(identity, g => {
-  g(m.from([1, 2, 3]))
+  g(m.periodic(1000).scan(c => c + 1, 0).map(i => h('div', i)))
   g(rootNode(identity, g => {
-    g(m.of('a'))
+    g(m.of(h('div', 'a')))
     g(rootNode(identity, g => {
-      g(m.of(true))
-      g(m.of(false))
+      g(m.of(h('input', {props: {type: 'checkbox', checked: true}})))
+      g(m.of(h('input', {props: {type: 'checkbox', checked: false}})))
     })())
-    g(m.of('b'))
+    g(m.of(h('div', 'b')))
   })())
-  g(m.from([3, 2, 1]))
+  g(m.periodic(1000).delay(500).scan(c => c - 1, 0).map(i => h('div', i)))
 })()
-tree$.observe(tree => console.log(JSON.stringify(tree)))
+
+const patch = snabbdom.init(['class', 'props', 'style'].map(
+  name => require('snabbdom/modules/' + name).default
+))
+tree$.reduce(patch, document.getElementById('root-node'))

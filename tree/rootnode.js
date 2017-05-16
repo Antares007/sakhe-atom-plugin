23ff1dfb96341cc207fc5d47f9739c72674b170a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-05-16 20:38:04 +0400

    A

--- tree/rootnode.js
+++ tree/rootnode.js
@@ -1,45 +1,27 @@
-// a :: ([a] -> b) -> [(a -> void), (void -> b)]
-function a (f) {
-  var as = []
-  return [
-    function g (a) { as.push(a) },
-    function r () { const as_ = as; as = []; return f(as_) }
-  ]
+function A (f) {
+  return function a (heart) {
+    const as = []
+    heart.call({ node: heart => { as.push(a(heart)) } }, as.push.bind(as))
+    return f(as)
+  }
 }
 
-module.exports = rootNode
-
-function rootNode (f, body) {
-  const [g, r] = a(f)
-  body(g)
-  return r
-}
+module.exports = A
 
 if (require.main === module) {
-  const identity = as => as
-  const tree = rootNode(identity, g => {
-    g(1)
-    g(rootNode(identity, g => {
-      g('a')
-      g(rootNode(identity, g => {
-        g(true)
-        g(false)
-      })())
-      g('b')
-    })())
-    g(2)
-  })()
-  console.log(JSON.stringify(tree, null, '  '))
-  // {
-  //   "0": 1,
-  //   "1": {
-  //     "0": "a",
-  //     "1": {
-  //       "0": true,
-  //       "1": false
-  //     },
-  //     "2": "b"
-  //   },
-  //   "2": 2
-  // }
+  console.log(A(as => as)(heart))
+  console.log(A(as => Object.assign({}, as))(heart))
+}
+
+function heart (push) {
+  push(1)
+  this.node(function (push) {
+    push('a')
+    this.node(function (push) {
+      push(true)
+      push(false)
+    })
+    push('b')
+  })
+  push(2)
 }
--- tree/snabbdomnode.js
+++ tree/snabbdomnode.js
@@ -1,27 +1,158 @@
-const snabbdom = require('snabbdom')
-const {curry2} = require('@most/prelude')
-const h = snabbdom.h
+const debug = require('debug')
 
-const m = require('most')
-const rootNode = require('./rootnode')
-const setKetByIndex = (vnode, i) => Object.assign({}, vnode, {key: i})
-const identity = curry2(m.combineArray)(
-  (...as) => h('div', as.map(setKetByIndex))
-)
-const tree$ = rootNode(identity, g => {
-  g(m.periodic(1000).scan(c => c + 1, 0).map(i => h('div', i)))
-  g(rootNode(identity, g => {
-    g(m.of(h('div', 'a')))
-    g(rootNode(identity, g => {
-      g(m.of(h('input', {props: {type: 'checkbox', checked: true}})))
-      g(m.of(h('input', {props: {type: 'checkbox', checked: false}})))
-    })())
-    g(m.of(h('div', 'b')))
-  })())
-  g(m.periodic(1000).delay(500).scan(c => c - 1, 0).map(i => h('div', i)))
-})()
+function A (f) {
+  return function a (heart) {
+    const as = []
+    heart.call({ node: heart => { as.push(a(heart)) } }, a => as.push(a))
+    return f(as)
+  }
+}
 
-const patch = snabbdom.init(['class', 'props', 'style'].map(
-  name => require('snabbdom/modules/' + name).default
-))
-tree$.reduce(patch, document.getElementById('root-node'))
+main()
+function main () {
+  const snabbdom = require('snabbdom')
+  const h = snabbdom.h
+  const h$ = (...args) => m.of(h(...args))
+  const m = require('most')
+
+  const VdomTree = A((vnode$s) => {
+    if (vnode$s.some(x => !x || !x.source || !x.source.run)) {
+      throw TypeError(VdomTree.toString())
+    }
+    const vnode$ = m.combineArray((...vnodes) => {
+      const vnode = h('div.node', vnodes.map((x, i) => {
+        if (x && typeof x.sel === 'string') {
+          return Object.assign(x, {key: i})
+        }
+        return h('div.type-error', {key: i}, x + '')
+      }))
+      return vnode
+    }, vnode$s)
+    return vnode$
+  })
+
+  const heart = function heart (push, $, path) {
+    const btn = n => h$('button', { on: { click: 'action' + n } }, [
+      h('h1', 'Hi' + n)
+    ])
+    $.observe(debug(path))
+    push(btn(path))
+    this.node(function (push, $, path) {
+      $.observe(debug(path))
+      push(btn(path))
+      this.node(function (push, $, path) {
+        $.observe(debug(path))
+        push(btn(path))
+      })
+    })
+    this.node(function (push, $, path) {
+      $.observe(debug(path))
+      push(btn(path))
+    })
+  }
+  const actionModule = require('../lib/drivers/snabbdom/actionModule')
+
+  const vnode$ = VdomTree(
+    addInputs(actionModule.action$,
+      addPathToNodeInput(
+        mapInputs(args => [
+          ...args.slice(0, 2),
+          args[2] ? '/' + args[2].join('/') : '/'
+        ],
+          heart
+        )
+      )
+    )
+  )
+
+  SnabbdomRootNode(document.getElementById('root-node'), vnode$, [
+    ...['class', 'props', 'style']
+      .map(name => require('snabbdom/modules/' + name).default),
+    actionModule
+  ])
+
+  function mapInputs (f, heart) {
+    return function (...args) {
+      heart.apply({
+        node: heart => { this.node(mapInputs(f, heart)) }
+      }, f(args))
+    }
+  }
+  function addInputs (xs, heart) {
+    return function (...args) {
+      const node = (xs, heart) => { this.node(addInputs(xs, heart)) }
+      heart.apply({node: node.bind(this, xs)}, args.concat(xs))
+    }
+  }
+  function addPathToNodeInput (heart, path = []) {
+    return function (...args) {
+      const node = (heart, i) => {
+        this.node(addPathToNodeInput(heart, [...path, i]))
+      }
+      var i = 0
+      heart.apply({ node: heart => { node(heart, i++) } }, args.concat([path]))
+    }
+  }
+}
+
+function SnabbdomRootNode (elm, vnode$, modules) {
+  const patch = require('snabbdom').init(modules)
+  vnode$
+    .tap(function () {
+      var ident = ''
+      const logVnode = vnode => {
+        debug('patch')(ident + vnode.sel + '(' + (vnode.scopes ? vnode.scopes.toString() : '') + ')')
+      }
+      return function visitChildren (vnode) {
+        logVnode(vnode)
+        if (vnode.children) {
+          ident = ident + '  '
+          vnode.children.forEach(visitChildren)
+          ident = ident.slice(-2)
+        }
+      }
+    }())
+    .tap(debug('patch'))
+    .reduce(patch, elm)
+  // const Nil = {
+  //   toString () {
+  //     return 'Nil'
+  //   }
+  // }
+  // const Cons = (head, tail) => ({
+  //   head,
+  //   tail,
+  //   toString () {
+  //     return 'cons(' + this.head + ', ' + tail.toString() + ')'
+  //   }
+  // })
+  // function isolateSink ($, scope) {
+  //   return $.map(function mapVnode (vnode) {
+  //     const scopes = vnode.scopes
+  //     const children = vnode.children
+  //     return Object.assign({}, vnode, {
+  //       scopes: scopes ? Cons(scope, scopes) : Cons(scope, Nil),
+  //       children: children ? children.map(mapVnode) : children
+  //     })
+  //   })
+  // }
+  //
+  // function isolateSource ($, scope) {
+  //   return $.filter(({vnode}) => vnode.scopes && vnode.scopes.head === scope)
+  //     .tap(debug('isolateSource' + scope + ':'))
+  //     .map((x) => Object.assign({}, x, {
+  //       vnode: Object.assign({}, x.vnode, {
+  //         scopes: x.vnode.scopes.tail
+  //       })
+  //     }))
+  // }
+
+  // function SnabbdomRootNode () {}
+  //
+  // const snabbdomnode = SnabbdomRootNode(elm, (data, children) => h('div', data, children), function (g, $) {
+  //   g(snabbdomnode((data, children) => h('div', data, children), function (g, $) {
+  //
+  //   }))
+  // })
+  //
+}

9f0c22899b08cfe54525a338c3528a12e2842f2f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-06-19 22:35:23 +0400

    H

---
+++ tree/n.js
@@ -0,0 +1,133 @@
+const ATree = require('./atree')
+const m = require('most')
+const h = require('snabbdom').h
+const toVnode = require('snabbdom/tovnode').default
+const actionModule = require('../lib/drivers/snabbdom/actionModule')
+const patch = require('snabbdom').init([
+  ...['class', 'props', 'style', 'attributes'].map(name => require('snabbdom/modules/' + name).default),
+  actionModule
+])
+
+const patchBark = (rootNode, vnode$) => vnode$.reduce(patch, toVnode(rootNode))
+
+const {Cons, nil} = require('./list')
+
+const to$ = (x) =>
+  x instanceof m.Stream
+  ? x
+  : x && typeof x === 'object' && Object.keys(x).some(key => x[key] instanceof m.Stream)
+  ? m.combineArray(function () {
+    return Object.keys(x).reduce((s, key, i) => {
+      s[key] = arguments[i]
+      return s
+    }, {})
+  }, Object.keys(x).map(key => to$(x[key])))
+  : m.of(x)
+
+const h$ = (sel, data, children, path) =>
+  m.combine(function (sel, data, children) {
+    data.key = path.head
+    data.path = path
+    return h(sel, data, children)
+  }, to$(sel), to$(data), to$(children))
+
+const H = (sel, data, pith, path = nil) => {
+  console.groupCollapsed(sel.toString(), path.toString(), JSON.stringify(data))
+  console.groupCollapsed('pith')
+  console.log(pith.toString())
+  console.groupEnd('pith')
+  var i = 0
+  const vdom$ = ATree(
+    vnode$s => h$(sel, data, m.combineArray((...children) => children, vnode$s), path),
+    push => (sel, data, children) => push(
+      (typeof children === 'function' ? H : h$)(sel, data, children, new Cons(i++, path))
+    ),
+    function (push) {
+      pith(push, path.toString(), m.empty())
+    }
+  )
+  console.groupEnd(sel.toString())
+  return vdom$
+}
+
+const repeat = (n, stream) =>
+  n <= 0
+  ? m.empty()
+  : n === 1
+  ? stream
+  : m.continueWith(() => repeat(n - 1, stream), stream)
+
+const animationFrame$ = require('./animation-frame').take(1000)
+const rootNode = document.getElementById('root-node')
+console.profile('run')
+patchBark(
+  rootNode,
+  H('div#root-node', {}, h => {
+    h('div', {style: css$`width: 100%; height: 50%;`}, Animation(3, 3))
+    h('div', {style: css$`width: 100%; height: 50%;`}, Animation(3, 3))
+  })
+).then(function log (vnode) {
+  if (!vnode.children) return console.log(vnode.sel, vnode.elm, JSON.stringify(vnode.data))
+  console.groupCollapsed(vnode.sel, vnode.elm, JSON.stringify(vnode.data))
+  vnode.children.forEach(log)
+  console.groupEnd(vnode.sel)
+}).then(() => console.profileEnd('run'))
+
+function Tree (d = 4, w = 2) { //eslint-disable-line
+  return (h, path, $) => {
+    h('h' + (d + 1), {}, path)
+    for (var i = 0; i < w; i++) {
+      if (d > 0) h('div', {style: {paddingLeft: '20px'}}, Tree(d - 1, w))
+    }
+  }
+}
+
+function Animation () { //eslint-disable-line
+  return (h, x) => {
+    const cycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + (0.1), 0)
+    const sin$ = cycle$.map(i => Math.sin(i))
+    const cos$ = cycle$.map(i => Math.cos(i))
+    h('div.contentContainer', {
+      style: css$`
+        width: 100%; height: 100%;
+        border: 5px white solid;
+        overflow: hidden;
+        background-color: #FFFF66;
+        `,
+      props: {
+        width: '300px'
+      }
+    }, (h, x) => {
+      h('div', {
+        style: css$`
+          position: relative; width: 300px; height: 300px;
+          left: ${cos$.map(i => Math.floor(225 + 200 * i + 0.5))}px;
+          top: ${sin$.map(i => Math.floor(125 + 100 * i + 0.5))}px;
+        `
+      }, h => h('img', { attrs: css$`src: ../tree/donut.png; width: 300px; height: 300px;` }))
+    })
+  }
+}
+
+function camelCase (str) {
+  const [first, ...last] = str.split('-')
+  return first + last.reduce((s, a) => s + a[0].toUpperCase() + a.slice(1), '')
+}
+
+function css (str) {
+  return str.split(/;|\n/).reduce((s, kvStr) => {
+    const [keyStr, valueStr] = kvStr.split(':')
+    const key = camelCase(keyStr.trim())
+    if (key === '' || key.startsWith('//')) return s
+    s[key] = valueStr.trim()
+    return s
+  }, {})
+}
+
+function css$ (strings, ...exprs) {
+  if (exprs.length === 0) return m.of(css(strings[0]))
+  return m.combineArray(
+    (...exprs) => css(strings.slice(1).reduce((rez, s, i) => rez + exprs[i] + s, strings[0])),
+    exprs.map(x => x instanceof m.Stream ? x : m.of(x))
+  )
+}
--- tree/test.js
+++ tree/test.js
@@ -1,13 +1,16 @@
 const H = require('./h')
 const m = require('most') //eslint-disable-line
+const animationFrame$ = require('./animation-frame')
 
-H('div#root-node.a', {}, Animation())
-
+H('div#root-node.a', {}, Counter())
 function Animation () { //eslint-disable-line
-  return ({h, path, $}) => {
+  return ({h}) => {
+    const cycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + (0.05), 0)
+    const sin$ = cycle$.map(i => Math.sin(i))
+    const cos$ = cycle$.map(i => Math.cos(i))
     h('div.contentContainer', {
       style: css$`
-        width: 100%;height: 100%;
+        width: 100%; height: 100%;
         border: 5px white solid;
         overflow: hidden;
         background-color: #FFFF66;
@@ -15,11 +18,8 @@ function Animation () { //eslint-disable-line
       props: {
         width: '300px'
       }
-    }, ({h, animationFrame$}) => {
-      const cycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + (0.3), 0)
-      const sin$ = cycle$.map(i => Math.sin(i))
-      const cos$ = cycle$.map(i => Math.cos(i))
-      const attrs = css(`src: ../tree/donut.png;width: 300px;height: 300px`)
+    }, ({h}) => {
+      const attrs = css(`src: ../tree/donut.png; width: 300px; height: 300px`)
       h('img', {
         style: css$`
           position: relative
@@ -42,21 +42,23 @@ function Tree (d = 4, w = 2) { //eslint-disable-line
   }
 }
 
-function Counter (d = 4) { //eslint-disable-line
+function Counter (d = 1) { //eslint-disable-line
   return ({h, action$, animationFrame$}) => {
     const sum$ = action$(-1).merge(action$(+1))
       .scan((sum, x) => sum + x.action, 0)
-    const picycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + 0.01, 0)
+    const picycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + 0.1, 0)
     const sin$ = picycle$.map(i => Math.sin(i))
     const cos$ = picycle$.map(i => Math.cos(i))
     const color$ = wave$ => wave$.map(i => 100 + d * 20 + Math.floor(30 * i))
-
+    const r = 10
     h('div', {style: {padding: '5px 10px', textAlign: 'center'}}, ({h}) => {
       h('button', {
         on: {click: +1},
         style: css$`
           position: relative
           border-radius: ${sin$.map(i => Math.abs(Math.floor(i * 20)))}px
+          // left: ${sin$.map(i => Math.floor(r * i))}px
+          // top: ${cos$.map(i => Math.floor(r * i))}px
           backgroundColor: rgb(255, ${color$(sin$)}, ${color$(cos$)})
         `
       }, ({h}) => {
@@ -69,6 +71,8 @@ function Counter (d = 4) { //eslint-disable-line
         style: css$`
           position: relative
           border-radius: ${cos$.map(i => Math.abs(Math.floor(i * 20)))}px
+          // left: ${cos$.map(i => Math.floor(r * i))}px
+          // top: ${sin$.map(i => Math.floor(r * i))}px
           backgroundColor: rgb(${color$(cos$)}, ${color$(sin$)}, 255)
         `
       }, ({h}) => {
@@ -129,7 +133,7 @@ function css (str) {
   return str.split(/;|\n/).reduce((s, kvStr) => {
     const [keyStr, valueStr] = kvStr.split(':')
     const key = camelCase(keyStr.trim())
-    if (key === '') return s
+    if (key === '' || key.startsWith('//')) return s
     s[key] = valueStr.trim()
     return s
   }, {})

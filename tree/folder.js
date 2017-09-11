7e81f844274cb3288fa807efabe610b1c3e1f0ca
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-09-11 14:58:58 +0400

    folder needs state

--- tree/folder.js
+++ tree/folder.js
@@ -2,65 +2,40 @@ const debug = require('debug') // eslint-disable-line
 const css$ = require('./css$')
 const m = require('most')
 const mount = require('./mount')
-const {join} = require('path')
+const {join: pathJoin} = require('path')
 const watch$ = require('./watch$')
-const {hold} = require('@most/hold')
-const eq = (a, b) => a.length === b.length && !a.some((v, i) => b[i] !== v)
+// const {hold} = require('@most/hold')
+// const eq = (a, b) => a.length === b.length && !a.some((v, i) => b[i] !== v)
 
-mount(document.getElementById('root-node'), Folder(join(__dirname, '..')))
+mount(document.getElementById('root-node'), Folder(pathJoin(__dirname, '..')))
 
 function Folder (path) {
   return h => {
-    const state = {}
-    h.$.map(x => x.action).scan((s, [path, isOpen]) => {
-      s[path] = isOpen
-      return s
-    }, state).drain()
-    FolderRec(path, state)(h)
-  }
-}
-
-function FolderRec (path, state) {
-  return h => {
-    const change$ = hold(watch$(path))
-    const entry$ = (name) =>
-      change$
-        .filter(([p, c]) => p[name] || c[name])
-        .map(([p, c]) => [p[name], c[name]])
-    change$
-      .filter(([prev, curr]) => !eq(Object.keys(prev), Object.keys(curr)))
-      .map(([prev, curr]) => {
-
-      })
-
-    const entries$ = hold(watch$(path))
-      .map(([prev, curr]) => Object.keys(curr).map(name => ({name, stat: curr[name]})))
-      .map(entrs => entrs.map(
-        ({name, stat}) => ({ name, isDir: stat.isDirectory(), path: join(path, name) })
-      ))
     h(
       'ul',
       {style: css$` list-style-type: none; `},
-      entries$
-        .map(entries => Entries(entries, state))
+      watch$(path)
+        .map(([p, c]) => c)
+        .map(entries => Entries(path, entries))
         .flatMapError(err => m.of(h => h('li', h => h(err.message))))
-        .startWith(h => h('li', h => h('Loading...')))
     )
   }
 }
 
-function Entries (entries, state) {
+function Entries (basePath, entries) {
   return h => {
-    for (let i = 0; i < entries.length; i++) {
-      let {path, name, isDir} = entries[i]
+    for (let name in entries) {
+      let path = pathJoin(basePath, name)
+      let isDir = entries[name].isDirectory()
       let actClose = [path, false]
       let actOpen_ = [path, true]
       const openClose$ = h.$
         .filter(x => x.action === actClose || x.action === actOpen_)
         .map(x => x.action[1])
-        .startWith(!!state[path])
+        .startWith(false)
       h(
         'li',
+        {key: name},
         isDir
         ? h => h(
           'div',
@@ -68,7 +43,7 @@ function Entries (entries, state) {
             op
             ? h => {
               h('button', {on: {click: actClose}}, h => h('- ' + name))
-              h('div', {}, FolderRec(path, state))
+              h('div', {}, Folder(path))
             }
             : h => h('button', {on: {click: actOpen_}}, h => h('+ ' + name))
           ))

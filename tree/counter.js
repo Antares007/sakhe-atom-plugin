d90f5163f04c8909e558f7f4dd25fc2afd7a1696
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-09-24 11:33:08 +0400

    refactor h$

--- tree/counter.js
+++ tree/counter.js
@@ -14,7 +14,7 @@ const patch = require('snabbdom').init([
 const h$ = require('./create-h$')(action$)
 
 const animationFrame$ = require('./animation-frame').take(1500)
-const cycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + (0.3), 0)
+const cycle$ = animationFrame$.scan(i => i >= Math.PI * 2 ? 0 : i + (0.05), 0)
 const sin$ = cycle$.map(i => Math.sin(i))
 const cos$ = cycle$.map(i => Math.cos(i))
 
@@ -32,8 +32,8 @@ function Counter (d = 1) { // eslint-disable-line
         style: css$`
           position: relative; outline: none
           border-radius: ${sin$.map(i => Math.abs(Math.floor(i * 20)))}px
-          // left: ${cos$.map(i => Math.floor(r * i))}px
-          // top: ${sin$.map(i => Math.floor(r * i))}px
+          left: ${cos$.map(i => Math.floor(r * i))}px
+          top: ${sin$.map(i => Math.floor(r * i))}px
           backgroundColor: rgb(255, ${color$(sin$)}, ${color$(cos$)})
         `
       }, h => {
@@ -45,8 +45,8 @@ function Counter (d = 1) { // eslint-disable-line
         style: css$`
           position: relative; outline: none
           border-radius: ${cos$.map(i => Math.abs(Math.floor(i * 20)))}px
-          // left: ${sin$.map(i => Math.floor(r * i))}px
-          // top: ${cos$.map(i => Math.floor(r * i))}px
+          left: ${sin$.map(i => Math.floor(r * i))}px
+          top: ${cos$.map(i => Math.floor(r * i))}px
           backgroundColor: rgb(${color$(sin$)}, ${color$(cos$)}, 255)
         `
       }, h => {

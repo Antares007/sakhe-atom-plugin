751abccc2a5ddf2301fc877c7cda0010923a04cf
Archil Bolkvadze <archilbolkvadze@Archils-MacBook-Pro.local>
2017-09-29 18:14:02 +0400

    dcmap

--- tree/barks/state.js
+++ tree/barks/state.js
@@ -5,42 +5,51 @@ const id = a => a
 
 module.exports = { ArrayBark, ObjectBark }
 
-function CollectionBark (rmap, chain, pith, pmap = id) {
+function CollectionBark (pith, pmap = id, cmap = id) {
   return Bark(
-    r$s => m.mergeArray(r$s).map(rmap),
+    cmap(r$s => m.mergeArray(r$s)),
     pith,
     pith => function (leaf) {
       pmap(pith)(
-        (index, pith, pmap = id) => leaf(ObjectBark(pith, pmap).flatMap(chain(index))),
-        (index, pith, pmap = id) => leaf(ArrayBark(pith, pmap).flatMap(chain(index))),
-        (index, r) => leaf($(r).flatMap(chain(index)))
+        (chain, pith, pmap = id) => leaf(ObjectBark(pith, pmap).flatMap(chain)),
+        (chain, pith, pmap = id) => leaf(ArrayBark(pith, pmap).flatMap(chain)),
+        (chain, r) => leaf($(r).flatMap(chain))
       )
     }
   )
 }
 
 function ArrayBark (pith, pmap = id) {
+  const mkChain = index => r => $(index).map(index => s => {
+    var l = Math.max(s.length, index + 1)
+    var b = new Array(l)
+    for (var i = 0; i < l; i++) {
+      b[i] = i === index ? r(s[i]) : s[i]
+    }
+    return b
+  })
   return CollectionBark(
-    r => s => Array.isArray(s) ? r(s) : r([]),
-    index => r => $(index).map(index => s => {
-      var l = Math.max(s.length, index + 1)
-      var b = new Array(l)
-      for (var i = 0; i < l; i++) {
-        b[i] = i === index ? r(s[i]) : s[i]
-      }
-      return b
-    }),
     pith,
-    pmap
+    pith => (o, a, v) => pmap(pith)(
+      (index, pith, pmap = id) => o(mkChain(index), pith, pmap),
+      (index, pith, pmap = id) => a(mkChain(index), pith, pmap),
+      (index, r) => v(mkChain(index), r)
+    ),
+    dc => r$s => dc(r$s).map(r => s => Array.isArray(s) ? r(s) : r([]))
   )
 }
 
 function ObjectBark (pith, pmap = id) {
+  const mkChain = index => r => $(index)
+    .map(key => s => Object.assign({}, s, {[key]: r(s[key])}))
   return CollectionBark(
-    r => s => typeof s === 'object' && s !== null ? r(s) : r({}),
-    index => r => $(index).map(key => s => Object.assign({}, s, {[key]: r(s[key])})),
     pith,
-    pmap
+    pith => (o, a, v) => pmap(pith)(
+      (key, pith, pmap = id) => o(mkChain(key), pith, pmap),
+      (key, pith, pmap = id) => a(mkChain(key), pith, pmap),
+      (key, r) => v(mkChain(key), r)
+    ),
+    dc => r$s => dc(r$s).map(r => s => typeof s === 'object' && s !== null ? r(s) : r({}))
   )
 }
 

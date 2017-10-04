4520ad1e51a8e1b1884fe687f11281ba7b0d330a
Archil Bolkvadze <a.bolkvadze@.gmail.com>
2017-10-04 08:36:44 +0400

    simplify state

--- tree/barks/state.js
+++ tree/barks/state.js
@@ -5,60 +5,45 @@ const {async: subject, hold} = require('most-subject')
 const id = a => a
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 
-const aChain = index => r => s => {
+const CollectionBark = (pmap = id) => c => Bark(
+  m.mergeArray,
+  pith => function (m) {
+    pmap(pith)(
+      pmap => key => pith => m(ObjectBark(pmap)(pith).map(c(key))),
+      pmap => key => pith => m(ArrayBark(pmap)(pith).map(c(key))),
+      (key, r) => m($(r).map(c(key)))
+    )
+  }
+)
+
+const ArrayBark = pmap => CollectionBark(pmap)(index => r => s => {
+  var b
+  if (!Array.isArray(s)) {
+    b = (new Array(index + 1)).fill(void 0)
+    b[index] = r(void 0)
+    return b
+  }
   const os = s[index]
   const ns = r(os)
   if (os === ns && index < s.length) return s
   const l = Math.max(s.length, index + 1)
-  const b = new Array(l)
+  b = new Array(l)
   for (var i = 0; i < l; i++) {
     b[i] = i === index ? ns : s[i]
   }
   return b
-}
-const oChain = key => r => s => {
+})
+
+const ObjectBark = pmap => CollectionBark(pmap)(key => r => s => {
+  if (typeof s !== 'object' || s === null) return {[key]: r()}
   const os = s[key]
   const ns = r(os)
   return (os === ns ? s : Object.assign({}, s, {[key]: ns}))
-}
-const aInitState = s => Array.isArray(s) ? s : []
-const oInitState = s => typeof s === 'object' && s !== null ? s : {}
-
-const CollectionBark = (pmap = id) => Bark(
-  m.mergeArray,
-  pith => function (m) {
-    pmap(pith)(
-      pmap => rmap => pith => m(ObjectBark(pmap)(pith).map(rmap)),
-      pmap => rmap => pith => m(ArrayBark(pmap)(pith).map(rmap)),
-      (rmap, r) => m($(r).map(rmap))
-    )
-  }
-)
-
-const ArrayBark = (pmap = id) => CollectionBark(
-  pith => (o, a, v) => {
-    pmap(pith)(
-      pmap => index => o(pmap)(r => s => aChain(index)(r)(aInitState(s))),
-      pmap => index => a(pmap)(r => s => aChain(index)(r)(aInitState(s))),
-      (index, r) => v(r => s => aChain(index)(r)(aInitState(s)), r)
-    )
-  }
-)
-
-const ObjectBark = (pmap = id) => CollectionBark(
-  pith => (o, a, v) => {
-    pmap(pith)(
-      pmap => key => o(pmap)(r => s => oChain(key)(r)(oInitState(s))),
-      pmap => key => a(pmap)(r => s => oChain(key)(r)(oInitState(s))),
-      (key, r) => v(r => s => oChain(key)(r)(oInitState(s)), r)
-    )
-  }
-)
+})
 
 const stateRing = state$ => pith => {
   const select = (key, $ = state$.skipRepeats().multicast()) =>
-    $.map(s => s[key])
-      .filter(a => typeof a !== 'undefined')
+    $.map(s => s[key]).filter(a => typeof a !== 'undefined')
   return (obj, arr, val) => pith(
     (pmap = id) => key => obj(compose(stateRing(select(key, state$)), pmap))(key),
     (pmap = id) => key => arr(compose(stateRing(select(key, state$)), pmap))(key),
@@ -85,9 +70,7 @@ if (require.main === module) {
     // v('key', m.of(s => 'value$'))
     o()('a')((o, a, v, state$) => {
       o()('a')((o, a, v, state$) => {
-        o()('a')((o, a, v, state$) => {
-          v('key', s => 'value')
-        })
+        v('key', s => 'value')
       })
     })
     a()('array')((o, a, v, select) => {
@@ -97,9 +80,9 @@ if (require.main === module) {
       v(1, s => 41)
       v(2, s => s - 1)
     })
-    select('array').observe(console.log.bind(console))
+    // select('array').observe(console.log.bind(console))
   })
-  // .tap(x => console.log(x))
+  .tap(x => console.log(x))
   .take(10)
   .drain()
 }

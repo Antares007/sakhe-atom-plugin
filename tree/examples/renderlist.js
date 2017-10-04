cdfa13837d6e14abe55198065dcaa19ad1e16c6f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-05 02:46:49 +0400

    dlist

--- tree/examples/renderlist.js
+++ tree/examples/renderlist.js
@@ -1,4 +1,4 @@
-// const m = require('most')
+const m = require('most')
 const {ReducerBark} = require('../barks/state')
 const PatchBark = require('../barks/patch')
 const H$ = require('../barks/h$')
@@ -16,39 +16,45 @@ const aframe$ = require('./animation-frame').multicast()
 //   initial,
 //   stream
 // )
-
+const eq = (a, b) => {
+  if (a === b) return true
+  if (typeof a === 'object' && typeof b === 'object' && a !== null && b !== null) {
+    if (Array.isArray(b) && b.length === a.length && !b.some((li, i) => !eq(a[i], li))) return true
+    const akeys = Object.keys(a)
+    const bkeys = Object.keys(b)
+    return bkeys.length === akeys.length && !bkeys.some(key => !eq(a[key], b[key]))
+  }
+  return false
+}
 PatchBark()(document.getElementById('root-node'))(h => {
-  // h(aframe$.scan(a => a + 1, 0))
-  // var i = 0
-  const node = (sel, data, list$, keymap, eq, pithmap) => {
-    // const key = 'node' + i++
+  const node = (sel, data, list$, keymap, eq, limap) => {
     const state$ = ReducerBark()()(s => {
-      s.arr('vnode$s')(s => {
-        for (let i = 0; i < 5; i++) {
-          s(i, s =>
-            H$(h.apiRing)(sel, data)(h => {
-              h(aframe$.scan(a => a + i / 1000, 0)
-                .map(i => Math.floor(i * 10 + 0.5))
-                .skipRepeats()
-              )
-              h('a')
-            })
-          )
-        }
-      })
+      s('lis', list$.map(list => s => list.reduce((s, li) => {
+        s[li.name] = li
+        return s
+      }, {})).map(s.keepEqs(_ => ({}))))
       s(
         'pith',
-        s.select('vnode$s')
-          .skipRepeatsWith((a, b) => a.length === b.length)
-          .map(vnode$s => () => h => {
-            vnode$s.forEach(h.vnode)
+        s.select('lis')
+          .skipRepeatsWith((a, b) => eq(Object.keys(a).sort(), Object.keys(b).sort()))
+          .map(lis => () => h => {
+            h('ul', h => {
+              for (let name in lis) {
+                h('li', {key: name}, s.select(name, s.select('lis')).skipRepeats().map(li => h => {
+                  h(li.name)
+                  h(li.atime)
+                  h(Math.random())
+                }))
+              }
+            })
             h(Math.random())
           })
       )
     }).multicast()
-    h(sel, data, state$.map(s => s.pith).filter(Boolean))
+    h(sel, data, state$.map(s => s.pith).filter(Boolean).skipRepeats())
     return state$
   }
+
   const list$ = watch$(__dirname)
     .map(es => Object.keys(es).map(key => {
       const stat = es[key]
@@ -60,7 +66,7 @@ PatchBark()(document.getElementById('root-node'))(h => {
       return {name, size, mode, atime, isDir}
     }))
 
-  h(list$.map(x => JSON.stringify(x)))
+  // h(list$.map(x => JSON.stringify(x)))
   node(
     'div',
     {},
@@ -74,8 +80,10 @@ PatchBark()(document.getElementById('root-node'))(h => {
       a.atime === b.atime &&
       a.isDir === b.isDir
     ),
-    li => h => h(li.name)
+    li => H$(h.apiRing)('li')(h => h(li.name))
   )
+  // .tap(x => console.log(x))
+  // .drain()
 })
   // .debounce(100)
   // .tap(x => x.log(x))

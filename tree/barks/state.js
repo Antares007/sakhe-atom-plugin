a8364f59d85435e1318044c140061b3a41e6957a
Archil Bolkvadze <a.bolkvadze@.gmail.com>
2017-10-05 02:46:49 +0400

    dlist

--- tree/barks/state.js
+++ tree/barks/state.js
@@ -15,18 +15,14 @@ const eq = (a, b) => {
   }
   return false
 }
-
+const c = (ft, k) => r => o => Object.assign(ft(), o, {[k]: r(o && o[k])})
 const ABark = (pmap = id) => (ft = () => ({})) => Bark(
   m.mergeArray,
   pith => function (m) {
-    const c = (t, k) => r => o => {
-      const a = r(o && o[k])
-      return Object.assign(t, o, {[k]: a})
-    }
     pmap(pith)(
-      pmap => key => pith => m(ABark(pmap)(() => ({}))(pith).map(c(ft(), key))),
-      pmap => key => pith => m(ABark(pmap)(() => ([]))(pith).map(c(ft(), key))),
-      (key, r) => m($(r).map(c(ft(), key)))
+      pmap => key => pith => m(ABark(pmap)(_ => ({}))(pith).map(c(ft, key))),
+      pmap => key => pith => m(ABark(pmap)(_ => ([]))(pith).map(c(ft, key))),
+      (key, r) => m($(r).map(c(ft, key)))
     )
   }
 )
@@ -35,8 +31,8 @@ const ArrayBark = pmap => ABark(pmap)(() => ([]))
 const ObjectBark = pmap => ABark(pmap)(() => ({}))
 
 const stateRing = state$ => pith => {
-  const select = (key, $ = state$.skipRepeats().multicast()) =>
-    $.map(s => s[key]).filter(a => typeof a !== 'undefined')
+  const select = (key, $ = state$) =>
+    $.filter(a => typeof a !== 'undefined' && a !== null).map(s => s[key]).skipRepeats()
   return (obj, arr, val) => pith(
     (pmap = id) => key => obj(compose(stateRing(select(key, state$)), pmap))(key),
     (pmap = id) => key => arr(compose(stateRing(select(key, state$)), pmap))(key),
@@ -44,31 +40,38 @@ const stateRing = state$ => pith => {
     select
   )
 }
+const keepEqs = ft => r => o => {
+  const a = r(o)
+  if (typeof a === 'object' && a !== null && typeof o === 'object' && o !== null) {
+    return Object.keys(a).reduce((s, key) => {
+      s[key] = eq(o[key], a[key]) ? o[key] : a[key]
+      return s
+    }, ft())
+  }
+  return a
+}
 const apiRing = pith => (obj, arr, val, select) => {
   const s = (...args) => val(...args)
   s.select = select
   s.obj = obj(apiRing)
   s.arr = arr(apiRing)
   s.apiRing = apiRing
+  s.keepEqs = keepEqs
   pith(s)
 }
 const ReducerBark = (pmap = apiRing) => (initState = {}, type = ObjectBark) => (pith) => {
   const state$ = hold(1, subject())
   return type(compose(stateRing(state$), pmap))(pith)
-    .scan((s, r) => r(s), initState)
-    .skip(1)
-    .skipRepeats()
-    .flatMapEnd(() => { state$.complete(); return m.empty() })
+    .scan((s, r) => r(s), initState).skip(1)
     .tap(state$.next.bind(state$))
+    .flatMapEnd(() => { state$.complete(); return m.empty() })
     .multicast()
 }
 
-module.exports = { ArrayBark, ObjectBark, ReducerBark, eq }
+module.exports = { ABark, ArrayBark, ObjectBark, ReducerBark, eq }
 
 if (require.main === module) {
   ReducerBark()()(s => {
-    // v('key', m.of(s => 'value$'))
-    // s('key', s => 'value')
     s.obj('a')(s => {
       s.obj('a')(s => {
         s('key', s => 'value')
@@ -80,7 +83,7 @@ if (require.main === module) {
       s(2, s => s - 1)
     })
   })
-  .tap(x => console.log(x))
   .take(10)
+  .tap(x => console.log(x))
   .drain()
 }

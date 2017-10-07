aa8f5897a58aea2fcfe20284b155371724fe485a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-07 09:48:08 +0400

    dlist needs better render

--- tree/examples/renderlist.js
+++ tree/examples/renderlist.js
@@ -1,47 +1,59 @@
-const debug = require('debug')
-// const m = require('most')
-// const id = a => a
-// const watch$ = require('./watch$')
+const debug = require('debug') // eslint-disable-line
+const m = require('most') // eslint-disable-line
+const watch$ = require('./watch$')
 const eq = require('../eq')
+const H$ = require('../barks/h$') // eslint-disable-line
+const {Cons} = require('../list') // eslint-disable-line
+const {join: pathJoin} = require('path') // eslint-disable-line
+// const id = a => a
 const PatchBark = require('../barks/patch')
 
-const list$ = require('./watch$')(__dirname)
-  .map(es => Object.keys(es).map(key => {
-    const stat = es[key]
-    const name = key
-    const size = stat.size
-    const mode = stat.mode
-    const mtime = stat.mtime.getTime()
-    return {name, size, mode, mtime, modestr: mode.toString(8)}
-  })).tap(debug('list$'))
-
-PatchBark()(document.getElementById('root-node'))(h => {
-  const rez$ = h.n('div', {}, {})((s, action$) => {
-    s.put('lis', list$.map(list => list.reduce((s, li) => {
-      s[li.name] = li
-      return s
-    }, {})).tap(debug('lis to put')))
-    s.put('return', 'result')
+const Folder = path => h => { // eslint-disable-line
+  h.n('div', {}, {})((s, action$) => {
+    s.put('lis', watch$(path).map(es =>
+      Object.keys(es).reduce((s, key) => {
+        const {size, mode, mtime} = es[key]
+        s[key] = {size, mode, mtime, isDir: !!(mode & parseInt('040000', 8))}
+        return s
+      }, {})
+    ))
+    // s.select(['lis']).scan((oldLis, lis) => {
+    //   const newLis = {}
+    //   for (let name in lis) {
+    //     newLis[name] =
+    //   }
+    //   return newLis
+    // }, {})
     return s.select(['lis'])
       .filter(lis => Object.keys(lis).length > 0)
       .skipRepeatsWith((a, b) => eq(Object.keys(a).sort(), Object.keys(b).sort()))
       .map(lis => (h, select) => {
         h('ul', h => {
           for (let name in lis) {
-            h('li', {key: name}, select(['lis', name]).map(li => h => {
+            h('li', {key: name, path: h.path}, select(['lis', name]).map(li => h => {
               h(name)
               h(' - ')
-              h(li.modestr)
-              h(' - ')
               h(Math.random())
-              // h(new Date(li.mtime).toLocaleString())
             }))
           }
+          h(Math.random())
         })
-        h(Math.random())
       })
   })
-  h(rez$.startWith('hi'))
+}
+
+PatchBark()(document.getElementById('root-node'))(h => {
+  h(h.$.tap(debug('root')).map(x => x.action).startWith('n/a'))
+  h.n('div.a')((s, action$) => {
+    s.put('count', action$.map(x => x.action).scan((s, a) => s + a, 0))
+    return (h, select) => {
+      h('button', {on: {click: +1}}, h => h('+'))
+      h('button', {on: {click: -1}}, h => h('-'))
+      h(select(['count']))
+    }
+  })
+  h('div', Folder(pathJoin(__dirname, '..')))
+  // Folder(__dirname + '/..')
 })
   .drain()
 

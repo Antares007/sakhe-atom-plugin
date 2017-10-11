a7b74ce9dd4ed9457dc285596031209ad9ab3d75
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-11 08:42:56 +0400

    makelipith

--- tree/examples/folder.js
+++ tree/examples/folder.js
@@ -16,7 +16,7 @@ PatchBark(p => cssRing(apiRing(p)))(document.getElementById('root-node'))(
     pathJoin(__dirname, '../..'),
     require('./folder-state.json'),
     initState => fs.writeFile(
-      pathJoin(__dirname, '/folder-state.json'),
+      pathJoin(__dirname, 'folder-state.json'),
       JSON.stringify(initState),
       (err) => err && console.log(err)
     )
@@ -51,35 +51,36 @@ function Folder (path, initState = { initStates: {}, op: {} }, stateCb = () => {
         enter.val('op', opAction$.map(([name, isOpen]) => s =>
           Object.assign({}, s, {[name]: isOpen})
         ))
-
+        const makeLiPith = (name, stat) => (put) => {
+          let epath = pathJoin(path, name)
+          let actClose = [path, name, false]
+          let actOpen_ = [path, name, true]
+          const isOpen$ = select.path(['op', name])
+          const initState$ = select.path(['initStates', name]).take(1)
+          put.node('li', {key: name}, (put, select) => {
+            if (stat.isDirectory()) {
+              put.node('div', isOpen$.map(op => (
+                  op
+                  ? put => {
+                    put.node('button', {on: {click: actClose}}, put => put.text('- ' + name))
+                    put.node('div', {},
+                      initState$.map(initState =>
+                        Folder(epath, initState, state => stateProxy$.next([name, state]))
+                      )
+                    )
+                  }
+                  : put => put.node('button', {on: {click: actOpen_}}, put => put.text('+ ' + name))
+                ))
+              )
+            } else {
+              put.text(name)
+            }
+          })
+        }
         enter.val('pith', change$.map(dir => () => (put) => {
           debug(path.slice(0, __dirname.length) + '/pith')(dir)
           for (let name in dir) {
-            let stat = dir[name]
-            let epath = pathJoin(path, name)
-            let actClose = [path, name, false]
-            let actOpen_ = [path, name, true]
-            const isOpen$ = select.path(['op', name])
-            const initState$ = select.path(['initStates', name]).take(1)
-            put.node('li', {key: name}, (put, select) => {
-              if (stat.isDirectory()) {
-                put.node('div', isOpen$.map(op => (
-                    op
-                    ? put => {
-                      put.node('button', {on: {click: actClose}}, put => put.text('- ' + name))
-                      put.node('div', {},
-                        initState$.map(initState =>
-                          Folder(epath, initState, state => stateProxy$.next([name, state]))
-                        )
-                      )
-                    }
-                    : put => put.node('button', {on: {click: actOpen_}}, put => put.text('+ ' + name))
-                  ))
-                )
-              } else {
-                put.text(name)
-              }
-            })
+            makeLiPith(name, dir[name])(put)
           }
         }))
       })

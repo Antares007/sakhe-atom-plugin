442056aebdadb1079c9b25515972c7ce8e613660
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-12 22:06:44 +0400

    idk

--- tree/eq.js
+++ tree/eq.js
@@ -1,11 +1,23 @@
-const eq = (a, b) => {
+const eq = (a, b, iq = Infinity) => {
+  if (iq === 0) return false
   if (a === b) return true
-  if (typeof a === 'object' && typeof b === 'object' && a !== null && b !== null) {
-    if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime()
-    if (Array.isArray(b) && b.length === a.length && !b.some((li, i) => !eq(a[i], li))) return true
+  if (
+    typeof a === 'object' && typeof b === 'object' &&
+    a !== null && b !== null
+  ) {
+    if (
+      a instanceof Date && b instanceof Date
+    ) return a.getTime() === b.getTime()
+    if (
+      Array.isArray(b) && b.length === a.length &&
+      !b.some((li, i) => !eq(a[i], li, iq - 1))
+    ) return true
     const akeys = Object.keys(a)
     const bkeys = Object.keys(b)
-    return bkeys.length === akeys.length && !bkeys.some(key => !eq(a[key], b[key]))
+    return (
+      bkeys.length === akeys.length &&
+      !bkeys.some(key => !eq(a[key], b[key], iq - 1))
+    )
   }
   return typeof a === 'function' && typeof a === 'function'
 }
--- tree/examples/renderlist.js
+++ tree/examples/renderlist.js
@@ -1,61 +1,49 @@
 const debug = require('debug') // eslint-disable-line
-const m = require('most') // eslint-disable-line
-const watch$ = require('./watch$')
-const eq = require('../eq')
-const vnodeBark = require('../barks/vnode') // eslint-disable-line
-const {Cons} = require('../list') // eslint-disable-line
-const {join: pathJoin} = require('path') // eslint-disable-line
-// const id = a => a
 const PatchBark = require('../barks/patch')
 
 const nRing = require('../rings/n')
 const apiRing = require('../rings/api')
 
-const Folder = path => nRing(h => { // eslint-disable-line
-  h.n('div', {}, {})((s, action$) => {
-    s.put('lis', watch$(path).map(es =>
-      Object.keys(es).reduce((s, key) => {
-        const {size, mode, mtime} = es[key]
-        s[key] = {size, mode, mtime, isDir: !!(mode & parseInt('040000', 8))}
-        return s
-      }, {})
-    ))
-    // s.select(['lis']).scan((oldLis, lis) => {
-    //   const newLis = {}
-    //   for (let name in lis) {
-    //     newLis[name] =
-    //   }
-    //   return newLis
-    // }, {})
-    return s.select(['lis'])
-      .filter(lis => Object.keys(lis).length > 0)
-      .skipRepeatsWith((a, b) => eq(Object.keys(a).sort(), Object.keys(b).sort()))
-      .map(lis => (h, select) => {
-        h('ul', h => {
-          for (let name in lis) {
-            h('li', {key: name, path: h.path}, select(['lis', name]).map(li => h => {
-              h(name)
-              h(' - ')
-              h(Math.random())
-            }))
-          }
-          h(Math.random())
-        })
-      })
-  })
-})
-PatchBark(p => nRing(apiRing(p)))(document.getElementById('root-node'))((put, select) => {
-  put.text(select.action$.tap(debug('root')).map(x => x.action + '').startWith('n/a'))
-  put.n('div.a', (enter, select) => {
-    // enter.val('count', select.action$.map(x => s => s + x.action)
-    //                                  .startWith(s => 0))
-    enter.put('count', select.action$.scan((c, x) => c + x.action, 0))
-    return (put, select) => {
+PatchBark(p => nRing()(apiRing(p)))(document.getElementById('root-node'))((put, select) => {
+  // put.text(select.action$.tap(debug('root')).map(x => x.action + '').startWith('n/a'))
+
+  put.n('div.a', {}, 'a', (enter, sselect, vselect) => {
+    enter.val('count', vselect.action$
+      .filter(x => typeof x.action === 'number')
+      .map(x => s => s + x.action)
+      .startWith(s => s || 0))
+    return showHideRing(put => {
       put.node('button', {on: {click: +1}}, put => put.text('+'))
       put.node('button', {on: {click: -1}}, put => put.text('-'))
-      put.text(select.path(['count']).map(n => n + ''))
-    }
+      put.text(sselect.path(['count']).map(n => n + ''))
+
+      put.n('div.b', {}, 'b', (enter, sselect, vselect) => {
+        enter.val('count', vselect.action$
+          .filter(x => typeof x.action === 'number')
+          .map(x => s => s + x.action)
+          .startWith(s => s || 0))
+        return showHideRing(put => {
+          put.node('button', {on: {click: +1}}, put => put.text('+'))
+          put.node('button', {on: {click: -1}}, put => put.text('-'))
+          put.text(sselect.path(['count']).map(n => n + ''))
+        })
+      })
+    })
   })
-  // h('div', Folder(pathJoin(__dirname, '..')))
 })
+  .tap(x => x.log())
   .drain()
+
+function showHideRing (pith) {
+  return function showHidePith (put, select) {
+    const showHide$ = select.action$
+      .filter(({action}) => action === showHide$)
+      .scan(b => !b, false).multicast()
+    put.node(
+      'button',
+      {on: {click: showHide$}},
+      showHide$.map(show => put => put.text(show ? 'hide' : 'show'))
+    )
+    put.node('div', showHide$.map(show => show ? pith : put => {}))
+  }
+}

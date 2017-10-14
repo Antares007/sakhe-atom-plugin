c4a593b451aa5b9825bdbc7aa075b7d1497c4111
Archil Bolkvadze <a.bolkvadze@.gmail.com>
2017-10-14 13:14:53 +0400

    omg

--- tree/rings/s.js
+++ tree/rings/s.js
@@ -1,10 +1,17 @@
 const debug = require('debug') // eslint-disable-line
+const m = require('most') // eslint-disable-line
 const {sync} = require('most-subject')
 const vnodeBark = require('../barks/vnode')
 const {ReducerBark} = require('../barks/state')
 const {Cons} = require('../list')
 const id = a => a
 const apiRing = require('../rings/api')
+const rchain = (ft, k) => r => a => {
+  const ak = a && a[k]
+  const bk = r(ak)
+  if (ak === bk) return a
+  return Object.assign(ft(), a, {[k]: bk})
+}
 
 const addActionRing = action$ => pith => (put, select) =>
   pith(Object.assign({}, put, {
@@ -13,76 +20,93 @@ const addActionRing = action$ => pith => (put, select) =>
     action$: action$.filter(x => x.vnode.data.path.endsWith(select.path))
   }))
 
-const svnodeBark = (select, initState = {initStates: {}}, reduce = id, spmap = apiRing) =>
-(pmap = id) => (sel, dta, key) => svpith => {
+const svnodeBark =
+(select, rproxy$ = {next: id}) =>
+(pmap = id, spmap = apiRing) =>
+(sel, dta, key, initState, ft) =>
+svpith => {
   const ringPath = select.path
   const data = select.$(dta).map(d => Object.assign({}, d, {path: ringPath, key}))
   const path = Cons(key, ringPath)
   const action$ = select.action$.filter(x => x.vnode.data.path.endsWith(path))
-  const vselect = Object.assign({}, select, {
-    path,
-    action$
-  })
-  const vnode$ = vnodeBark(addActionRing(action$))(sel, data, path)(
-    ReducerBark(id)(initState)((enter, select) => {
-      var vpith
+  const vselect = Object.assign({}, select, { path, action$ })
+  const rmap = (proxy$, f = id) => ({next: r => proxy$.next(s => f(r)(s))})
+
+  return vnodeBark(addActionRing(action$))(sel, data, path)(
+    ReducerBark(id)(initState, ft)((enter, sselect) => {
       const stateProxy$ = sync()
-      enter.val('initStates', stateProxy$)
+      enter.put(stateProxy$)
 
-      enter.obj(spmap)('state')((enter, sselect) => {
-        vpith = svpith(enter, sselect, vselect)
-      })
-
-      const selectPath = select.path
-      const chieldRing = pith => (put, select) => pmap(pith)(
-        Object.assign({}, put, {
-          node: (pmap = id) => put.node(p => chieldRing(pmap(p))),
-          snode: (pmap = id) => (sel, data, key) => svpith => {
-            put.vnode(
-              selectPath(['initStates', key]).take(1).map(initState =>
-                svnodeBark(
-                  select,
-                  initState,
-                  r => stateProxy$.next(
-                    s => Object.assign({}, s, {[key]: r(s && s[key])})
-                  ),
-                  spmap
-                )(pmap)(sel, data, key)(svpith)
-              ).switchLatest()
-            )
-          }
-        }),
-        select
+      const chieldRing = pith => (put, select) => {
+        const snode =
+        ft =>
+        (pmap = id, spmap) =>
+        (sel, dta, key) =>
+        svpith => {
+          put.vnode(
+            sselect.path([key]).take(1).map(initState =>
+              svnodeBark(
+                select, rmap(stateProxy$, rchain(ft, key))
+              )(
+                pmap, spmap
+              )(
+                sel, dta, key, initState, ft
+              )(
+                svpith
+              )
+            ).switchLatest()
+          )
+        }
+        return pmap(pith)(
+          Object.assign({}, put, {
+            node: (pmap = id) => put.node(p => chieldRing(pmap(p))),
+            onode: snode(_ => ({})),
+            anode: snode(_ => ([]))
+          }),
+          select
+        )
+      }
+      enter.put(
+        sselect.$(svpith(enter, sselect, vselect))
+          .map(chieldRing)
+          .map(pith => s => { s.pith = pith; return s })
       )
-      enter.val('pith', select.$(vpith).map(chieldRing).map(_ => () => _))
     })
-      .tap(debug(key + '/S'))
-      .tap(({state, initStates}) => reduce(lastState => (
-        lastState && lastState.state === state && lastState.initStates === initStates
-        ? lastState
-        : {state, initStates}
-      )))
-      .map(s => s.pith)
-      .filter(pith => typeof pith !== 'undefined')
-      .skipRepeats()
+    .tap(
+      (o => a => {
+        if (o === a) return
+        debug(key + '/next')(a)
+        o = a
+        rproxy$.next(() => {
+          if (!a.pith) return a
+          const b = Object.assign(ft(), a)
+          delete b.pith
+          return b
+        })
+      })(void 0)
+    )
+    .map(s => s.pith)
+    .filter(pith => typeof pith !== 'undefined')
+    .skipRepeats()
   )
-  return vnode$
 }
 
-const sRing = (initStates = {}, reduce = id) => pith => (put, select) => pith(
-  Object.assign({}, put, {
-    node: (pmap = id) => put.node(p => sRing(initStates, reduce)(pmap(p))),
-    snode: (pmap = id) => (sel, data, key) => svpith => {
-      put.vnode(
-        svnodeBark(
-          select,
-          initStates[key],
-          r => reduce(s => Object.assign({}, s, {[key]: r(s && s[key])}))
-        )(apiRing)(sel, data, key)(svpith)
-      )
-    }
-  }),
-  select
-)
+const sRing = pith => (put, select) => {
+  const snode =
+  ft =>
+  (pmap, spmap = apiRing) =>
+  (sel, dta, key, initState, proxy$) =>
+  svpith =>
+  put.vnode(
+    svnodeBark(select, proxy$)(pmap, spmap)(sel, dta, key, initState, ft)(svpith)
+  )
+  return pith(
+    Object.assign({}, put, {
+      onode: snode(() => ({})),
+      anode: snode(() => ([]))
+    }),
+    select
+  )
+}
 
 module.exports = sRing

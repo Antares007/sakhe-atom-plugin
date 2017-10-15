3376a65c4b88ff5e0d4ce53b7fc2fb97cfd740df
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-15 11:00:13 +0400

    folder

--- tree/examples/folder.js
+++ tree/examples/folder.js
@@ -1,92 +1,55 @@
-const m = require('most')
 const debug = require('debug')
 const watch$ = require('./watch$')
-const {ReducerBark} = require('../barks/state')
-
 const {join: pathJoin} = require('path')
 
 const eq = require('../eq')
 const PatchBark = require('../barks/patch')
 const cssRing = require('../rings/css')
 const apiRing = require('../rings/api')
-const fs = require('fs')
+const sRing = require('../rings/s')
 
-PatchBark(p => cssRing(apiRing(p)))(document.getElementById('root-node'))(
-  Folder(
-    pathJoin(__dirname, '../..'),
-    require('./folder-state.json'),
-    initState => fs.writeFile(
-      pathJoin(__dirname, 'folder-state.json'),
-      JSON.stringify(initState),
-      (err) => err && console.log(err)
-    )
-  )
-)
-.tap(debug('patch'))
-.drain()
-
-function Folder (path, initState = { initStates: {}, op: {} }, stateCb = () => {}) {
-  const {sync} = require('most-subject')
-
-  return (put, select) => {
-    const change$ = watch$(path)
-      .skipRepeatsWith(eq)
-      .multicast()
-    const opAction$ = select.action$
-      .filter(x => x.action[0] === path)
-      .map(x => x.action.slice(1))
-
-    put.node(
-      'ul',
-      {style: select.css$` list-style-type: none; `},
-      ReducerBark()(initState)((enter, select) => {
-        const stateProxy$ = sync()
-        enter.val(
-          'initStates',
-          stateProxy$
-            .skipRepeatsWith(eq)
-            .map(([name, state]) => s => Object.assign({}, s, {[name]: state}))
-        )
-
-        enter.val('op', opAction$.map(([name, isOpen]) => s =>
-          Object.assign({}, s, {[name]: isOpen})
-        ))
-        const makeLiPith = (name, stat) => (put) => {
-          let epath = pathJoin(path, name)
-          let actClose = [path, name, false]
-          let actOpen_ = [path, name, true]
-          const isOpen$ = select.path(['op', name])
-          const initState$ = select.path(['initStates', name]).take(1)
-          put.node('li', {key: name}, (put, select) => {
-            if (stat.isDirectory()) {
-              put.node('div', isOpen$.map(op => (
-                  op
-                  ? put => {
-                    put.node('button', {on: {click: actClose}}, put => put.text('- ' + name))
-                    put.node('div', {},
-                      initState$.map(initState =>
-                        Folder(epath, initState, state => stateProxy$.next([name, state]))
-                      )
-                    )
-                  }
-                  : put => put.node('button', {on: {click: actOpen_}}, put => put.text('+ ' + name))
-                ))
-              )
-            } else {
-              put.text(name)
-            }
-          })
-        }
-        enter.val('pith', change$.map(dir => () => (put) => {
-          debug(path.slice(0, __dirname.length) + '/pith')(dir)
-          for (let name in dir) {
-            makeLiPith(name, dir[name])(put)
+const Folder = path => (put, select) => {
+  const change$ = watch$(path)
+    .skipRepeatsWith(eq)
+    .multicast()
+  const opAction$ = select.action$
+    .filter(x => x.action[0] === path)
+    .map(x => x.action.slice(1))
+  put.onode('ul', { style: { listStyleType: 'none' } }, path, (enter, sselect, vselect) => {
+    enter.val('@', opAction$.map(([name, isOpen]) => s =>
+      Object.assign({}, s, {[name]: isOpen})
+    ).startWith(s => s || {}))
+    return change$.map(dir => put => {
+      for (let name in dir) {
+        let stat = dir[name]
+        let epath = pathJoin(path, name)
+        let actClose = [path, name, false]
+        let actOpen_ = [path, name, true]
+        const isOpen$ = sselect.path(['@', name])
+        put.node('li', {key: name}, (put, select) => {
+          if (stat.isDirectory()) {
+            put.node('div', isOpen$.map(op => (
+                op
+                ? put => {
+                  put.node('button', {on: {click: actClose}}, put => put.text('- ' + name))
+                  put.node('div', {}, Folder(epath))
+                }
+                : put => put.node('button', {on: {click: actOpen_}}, put => put.text('+ ' + name))
+              ))
+            )
+          } else {
+            put.text(name)
           }
-        }))
-      })
-        .tap(s => stateCb(s))
-        .map(s => s.pith)
-        .skipRepeats()
-    )
-  }
+        })
+      }
+    })
+  })
 }
+
+PatchBark(
+  p => cssRing(sRing(...(require('./initstate')('folder')))(apiRing(p)))
+)(
+  document.getElementById('root-node')
+)(
+  Folder(pathJoin(__dirname, '../..'))
+).tap(debug('patch')).drain()
